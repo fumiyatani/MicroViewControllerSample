f666474bebefd4cf087e87fb8405f3fdea9c91e3
tanifumiya <fumiyadeveloper@gmail.com>
2018-09-04 23:40:14 +0900

    Initial Commit

---
+++ MicroViewControllerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MicroViewControllerSample
+//
+//  Created by 谷郁也 on 2018/09/04.
+//  Copyright © 2018年 谷郁也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
